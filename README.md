65407c0becc3ed6b41a27f184f46cc041f259828
Luyi Song <songly91.6.1@gmail.com>
2018-04-23 23:07:15 -0400

    first polymer component

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+### Polymer Preview
+[Polymer 3.0](https://www.polymer-project.org/blog/2018-03-23-polymer-3-latest-preview).
+
 ### Install Polymer CLI
 ```
 npm install -g polymer-cli@next
--- src/hello-world/hw.js
+++ src/hello-world/hw.js
@@ -1,3 +1,7 @@
-console.log('haha');
-
-console.log(123);
+import {PolymerElement, html} from "@polymer/polymer/polymer-element.js"
+class CustomElement extends PolymerElement {
+    static get template() {
+        return `<h4>custom elssement from Polymer</h4>`
+    }
+}
+customElements.define('custom-element', CustomElement);
