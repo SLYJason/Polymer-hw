9a50405e2c2850d403f4d935741c4be4d542dd3c
Luyi Song <songly91.6.1@gmail.com>
2018-04-26 23:35:30 -0400

    data binding works

--- src/hello-world/hw.js
+++ src/hello-world/hw.js
@@ -62,3 +62,35 @@ class ConfigurableTagName extends PolymerElement{
     }
 }
 customElements.define('configurable-tag-name', ConfigurableTagName);
+
+class IronInput extends PolymerElement {
+    static get properties() {
+        return {
+            bindValue: {
+                type: String,
+                notify: true
+            }
+        }
+    }
+    static get template() {
+        return html`<input type="text" value="{{bindValue::input}}"> [[bindValue]]`;
+    }
+}
+customElements.define('iron-input', IronInput);
+
+class EditableName extends PolymerElement {
+    static get properties() {
+        return {
+            owner: {
+                type: String,
+                value: 'Luyi Song'
+            }
+        }
+    }
+    static get template() {
+        return `
+        Binding value: [[owner]] <iron-input bind-value="{{owner}}"></iron-input>
+        `;
+    }
+}
+customElements.define('editable-name', EditableName);
